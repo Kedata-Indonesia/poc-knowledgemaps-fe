759002aeb96f91ff6d2462afb7f8ec8b9a5974b1
Alfian Andi Nugraha <me@alfianandi.dev>
2024-01-13 14:20:31 +0700

    feat: setup title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo titleTemplate="%s | PT Kedata Indonesia Digital" />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import { Open_Sans } from "next/font/google";
 import { useState } from "react";
 import { Button, Input } from "react-daisyui";
 import { HiSearch } from "react-icons/hi";
+import { NextSeo } from "next-seo";
 
 const Home = () => {
   const [keyword, setKeyword] = useState("");
 
   return (
     <main className="flex w-full pt-20 h-screen">
+      <NextSeo title="Home" />
       <div className="mx-auto flex gap-4 max-w-lg w-full">
         <Input
           placeholder="Enter your search term"

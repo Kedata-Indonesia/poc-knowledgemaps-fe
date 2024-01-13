3dac6a7c28b888e9b5dcec93446cc2b2e1c4d7b2
Alfian Andi Nugraha <me@alfianandi.dev>
2024-01-13 12:24:18 +0700

    feat(tailwind): setup daisy ui

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Open_Sans } from "next/font/google";
+import { Button } from "react-daisyui";
 
 const openSans = Open_Sans({
   display: "swap",
@@ -7,7 +8,11 @@ const openSans = Open_Sans({
 });
 
 const Home = () => {
-  return <main className={openSans.className}>init program</main>;
+  return (
+    <main className={openSans.className}>
+      <Button color="primary">Open</Button>
+    </main>
+  );
 };
 
 export default Home;

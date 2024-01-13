27feddf0854ddf4aa38ccb1f994e253b2cf4e1cc
Alfian Andi Nugraha <me@alfianandi.dev>
2024-01-13 13:10:07 +0700

    feat: slicing homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { Open_Sans } from "next/font/google";
-import { Button } from "react-daisyui";
+import { useState } from "react";
+import { Button, Input } from "react-daisyui";
+import { HiSearch } from "react-icons/hi";
 
 const openSans = Open_Sans({
   display: "swap",
@@ -8,9 +10,21 @@ const openSans = Open_Sans({
 });
 
 const Home = () => {
+  const [keyword, setKeyword] = useState("");
+
   return (
-    <main className={openSans.className}>
-      <Button color="primary">Open</Button>
+    <main className="flex w-full pt-20 h-screen">
+      <div className="mx-auto flex gap-4 max-w-md w-full">
+        <Input
+          placeholder="Enter your search term"
+          className="w-full"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <Button color="primary" startIcon={<HiSearch />}>
+          Search
+        </Button>
+      </div>
     </main>
   );
 };

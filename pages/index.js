48c61c392eba23b1558d0852df09d5b96df52c54
Bagus TRIANURDIN <btstargroup@gmail.com>
2024-01-13 14:27:21 +0700

    feat(search): implement search page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Open_Sans } from "next/font/google";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { Button, Input } from "react-daisyui";
 import { HiSearch } from "react-icons/hi";
@@ -10,20 +11,29 @@ const openSans = Open_Sans({
 });
 
 const Home = () => {
+  const router = useRouter();
   const [keyword, setKeyword] = useState("");
 
   return (
     <main className="flex w-full pt-20 h-screen">
-      <div className="mx-auto flex gap-4 max-w-md w-full">
-        <Input
-          placeholder="Enter your search term"
-          className="w-full"
-          value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-        />
-        <Button color="primary" startIcon={<HiSearch />}>
-          Search
-        </Button>
+      <div className="mx-auto max-w-md w-full">
+        <form
+          className="flex gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            router.push(`/map?q=${keyword}`);
+          }}
+        >
+          <Input
+            placeholder="Enter your search term"
+            className="w-full"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+          <Button type="submit" color="primary" startIcon={<HiSearch />}>
+            Search
+          </Button>
+        </form>
       </div>
     </main>
   );
